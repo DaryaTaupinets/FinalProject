8772992e474dd679fb4b4ee9635449852331cbd7
admin <admin>
2020-10-07 21:54:51 +0300

    Changes in class Example

--- src/main/java/ru/mail/evmenova/springbootapp/model/Example.java
+++ src/main/java/ru/mail/evmenova/springbootapp/model/Example.java
@@ -2,6 +2,7 @@ package ru.mail.evmenova.springbootapp.model;
 
 public class Example {
     public static void main(String[] args) {
+        int a=1;
         System.out.println("Example");
     }
 }
